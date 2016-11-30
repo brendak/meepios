075daf7cf4a404fefdd6606bad400acdb394c87c
Brenda <hello@brendakaing.com>
2016-11-30 14:13:56 -0800

    First Commit

--- Meep/ViewController.swift
+++ Meep/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        print("button pressed")
+    }
+    
+    @IBAction func NextPressed(_ sender: UIButton) {
+        print("button pressed")
+
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = "Outlets are cool?"
+        answerLabel.text = "Yes!"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
